cbe923c6a113c7a66bb002f98fddc1ef504368df
ai2playgame <detective9684@gmail.com>
2020-07-07 00:46:55 +0900

    カスタムコントロールに依存関係プロパティ追加

--- WPF-TrushCan/customControl/CustomControlLib/Controls/LineGraph.cs
+++ WPF-TrushCan/customControl/CustomControlLib/Controls/LineGraph.cs
@@ -7,8 +7,24 @@ namespace CustomControlLib.Controls
     {
         static LineGraph()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(LineGraph), new FrameworkPropertyMetadata(typeof(LineGraph)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LineGraph),
+                new FrameworkPropertyMetadata(typeof(LineGraph)));
         }
+
+        // ---------------------------------------------------------------- //
+        //  依存関係プロパティ
+        // ---------------------------------------------------------------- //
+        public string SubText
+        {
+            get => (string)GetValue(SubTextProperty);
+            set => SetValue(SubTextProperty, value);
+        }
+        public static readonly DependencyProperty SubTextProperty =
+            DependencyProperty.Register(
+                nameof(SubText),
+                typeof(string),
+                typeof(LineGraph),
+                new UIPropertyMetadata(null));
 
         public LineGraph()
         {
