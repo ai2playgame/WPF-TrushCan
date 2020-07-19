1c97c2332e42cd24cc782b10db552305b58d6f40
ai2playgame <detective9684@gmail.com>
2020-07-19 19:51:00 +0900

    checkList

---
+++ WPF-TrushCan/CheckLists/Models/Person.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Text;
+
+namespace CheckLists.Models
+{
+    class Person : BindableBase
+    {
+        // ---------------------------------------------------------------- //
+        //  プロパティ
+        // ---------------------------------------------------------------- //
+        private int _ID;
+        public int ID
+        {
+            get => _ID;
+            set => SetProperty(ref _ID, value);
+        }
+        private string _firstName;
+        public string FirstName
+        {
+            get => _firstName;
+            set => SetProperty(ref _firstName, value);
+        }
+        private string _lastName;
+        public string LastName
+        {
+            get => _lastName;
+            set => SetProperty(ref _lastName, value);
+        }
+
+        public string ModelDisplay
+        {
+            get
+            {
+                string completeName = string.Format("{0} {1}", FirstName, LastName).PadRight(20);
+                return string.Format(
+                    "ID={0}, Name={1}", ID, completeName);
+            }
+        }
+
+    }
+}
---
+++ WPF-TrushCan/CheckLists/ViewModels/MainViewModel.cs
@@ -0,0 +1,83 @@
+﻿using CheckLists.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CheckLists.ViewModels
+{
+    class MainViewModel : BindableBase
+    {
+        // ---------------------------------------------------------------- //
+        //  プロパティ
+        // ---------------------------------------------------------------- //
+        
+        // ListViewに入れるData
+        public ObservableCollection<Person> People { get; set; } = new ObservableCollection<Person>();
+
+        public int IDCount { get; set; } = 0;
+        private string _firstName;
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                SetProperty(ref _firstName, value);
+                this.AddPersonCommand.RaiseCanExecuteChanged();
+            }
+        }
+        private string _lastName;
+        public string LastName 
+        {
+            get => _lastName;
+            set
+            {
+                SetProperty(ref _lastName, value);
+                this.AddPersonCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        // People追加用コマンド
+        public DelegateCommand AddPersonCommand { get; private set; }
+
+        // ---------------------------------------------------------------- //
+        //  コンストラクタ
+        // ---------------------------------------------------------------- //
+        public MainViewModel()
+        {
+            AddPersonCommand = new DelegateCommand(this.AddPerson, this.CanAddPerson);
+            People.Add(new Person() { ID = IDCount, FirstName = "Adam", LastName = "Eevee" });
+            IDCount++;
+        }
+
+        // ---------------------------------------------------------------- //
+        //  メソッド
+        // ---------------------------------------------------------------- //
+        
+        /// <summary>
+        /// Peopleに新たにPersonを追加
+        /// </summary>
+        private void AddPerson()
+        {
+            People.Add(new Person()
+            {
+                ID = IDCount,
+                FirstName = this.FirstName,
+                LastName = this.LastName
+            });
+            IDCount++;
+        }
+        /// <summary>
+        /// AddCommmandの実行可否を返す
+        /// </summary>
+        private bool CanAddPerson()
+        {
+            return !string.IsNullOrEmpty(this.FirstName) && !string.IsNullOrEmpty(this.LastName);
+        }
+
+    }
+}
